840154b2159ef675f371bc832c6a6208c31a5362
SID12g <sid12g@naver.com>
2024-11-10 08:38:52 +0900

    feat: Alert Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
 "use client";
 
+import Alert from "@/components/Alert";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <p>asdasdasd</p>
+      <p>asdasdasd</p>
+      <p>asdasdasd</p>
+      <p>asdasdasd</p>
+      <p>asdasdasd</p>
+      <p>asdasdasd</p>
+      {/* <Alert
+        title="로그인을 해주세요."
+        content="로그인 이후 서비스 사용이 가능합니다."
+      /> */}
+    </>
+  );
 }
---
+++ src/components/Alert.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import { Text } from "./Text";
+import { Colors } from "@/styles/color";
+import styled from "@emotion/styled";
+import { Box } from "./Box";
+import { Button } from "./Button";
+
+export default function Alert({
+  title,
+  content,
+}: {
+  title: string;
+  content: string;
+}) {
+  const PageWrap = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: rgb(0, 0, 0, 0.6);
+    position: absolute;
+    top: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  `;
+
+  const AlertWrap = styled.div`
+    background-color: ${Colors.dark_grey_color};
+    margin: 20px;
+    padding: 20px;
+    border-radius: 16px;
+  `;
+  return (
+    <PageWrap>
+      <AlertWrap>
+        <Text size="16px" weight="600" color={Colors.contrast_color}>
+          {title}
+        </Text>
+        <Box height="12px" borderColor="none" />
+        <Text size="12px" weight="500">
+          {content}
+        </Text>
+        <Box height="20px" borderColor="none" />
+        <Box borderColor="none">
+          <Button
+            width="100%"
+            padding="12px"
+            backgroundColor={Colors.contrast_color}
+            onClick={() => {}}
+          >
+            <Text size="12px" weight="500" color={Colors.primary_color}>
+              로그인
+            </Text>
+          </Button>
+          <Box width="20px" borderColor="none" />
+          <Button
+            width="100%"
+            padding="12px"
+            backgroundColor={Colors.warning_color}
+            onClick={() => {}}
+          >
+            <Text size="12px" weight="500" color={Colors.contrast_color}>
+              닫기
+            </Text>
+          </Button>
+        </Box>
+      </AlertWrap>
+    </PageWrap>
+  );
+}
